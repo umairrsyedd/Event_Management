c5560af05faea0fe69cf4e797ba79d4c4871d72d
Syed Umair <umair_developer@outlook.com>
2021-01-09 19:13:08 +0530

    Infinite Loop with Create Options Completed

--- main.py
+++ main.py
@@ -11,8 +11,15 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-
-# createClient(mycursor, mydb)
-# createVenue(mycursor, mydb)
-# createEvent(mycursor, mydb)
-selectAll(mycursor, mydb)
+while True:
+    print("Enter What Do You Want To Do\n")
+    choice = int(
+        input("1 : Create Client\n2 : Create Venue\n3 : Create Event\n"))
+    if choice == 1:
+        createClient(mycursor, mydb)
+    elif choice == 2:
+        createVenue(mycursor, mydb)
+    elif choice == 3:
+        createEvent(mycursor, mydb)
+    elif choice == 8:
+        break
