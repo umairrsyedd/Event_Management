07f9656cb95a42c9e40ad39092d8daf29eeb60b8
Syed Umair <umair_developer@outlook.com>
2021-01-12 12:22:50 +0530

    Added New Routes To Create Events

--- hello.py
+++ hello.py
@@ -4,7 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('createvenue.html')
+    return render_template('index.html')
+
+
+@app.route('/CreateVenue')
+def CreateClient():
+    return render_template('CreateVenue.html')
+
+
+@app.route('/CreateClient')
+def CreateVenue():
+    return render_template('CreateClient.html')
+
+
+@app.route('/CreateEvent')
+def CreateEvent():
+    return render_template('CreateEvent.html')
 
 
 if __name__ == '__main__':
